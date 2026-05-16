0f7f048e915f6979421b0ef1355997cedb84a41b
[dyad] <tripsevenguy@proton.me>
2026-05-16 22:49:40 +0930

    Reverted all changes back to version be631d8f8c288bcec782298ecd44ea0064ea4edf

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -23,7 +23,7 @@ import {
   DropdownMenuSeparator,
   DropdownMenuTrigger,
 } from "@/components/ui/dropdown-menu";
-import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/Avatar";
 import logo from '@/assets/hero-guy.jpg';
 import { cn } from '@/lib/utils';
 
